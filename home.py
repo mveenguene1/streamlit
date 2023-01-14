5a8b6dfb3321fd2b9d88858e21bfa54cd9e29d82
MVE ENGUENE JOSUE JOEL <83601143+mveenguene1@users.noreply.github.com>
2023-01-14 21:36:17 +0100

    Update home.py

--- home.py
+++ home.py
@@ -76,14 +76,9 @@ import streamlit as st
 # Add a selectbox to the sidebar:
 add_selectbox = st.sidebar.selectbox(
     'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone')
+    ('Current_time', 'Dayly_time')
 )
 
-# Add a slider to the sidebar:
-add_slider = st.sidebar.slider(
-    'Select a range of values',
-    0.0, 100.0, (25.0, 75.0)
-)
 
 
 
